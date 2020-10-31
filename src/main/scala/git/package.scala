1219e5d7bdebbb94b0376eb63a7be4aaacb28d66
vera.miroshnichenko <vevera.miroshnichenko@pimpay.ru>
2020-11-01 00:21:43 +0300

    refactoring

--- src/main/scala/git/package.scala
+++ src/main/scala/git/package.scala
@@ -1,11 +1,10 @@
 import domain.{Dag, GitRepoSettings, Project}
-import zio.{Has, Ref, Task, ZIO, ZLayer}
+import zio._
 import io.circe._
 import io.circe.generic.auto._
 import io.circe.syntax._
 import sttp.client3._
 
-import scala.util.Try
 
 
 package object git {
@@ -13,33 +12,30 @@ package object git {
 
   trait Service {
     def syncDag(dag: Dag, gitRepoSettings: GitRepoSettings): Task[String]
-  }
 
+    def createActions(dag: Dag, gitRepoSettings: GitRepoSettings) : UIO[Seq[Action]]
+    def createPayload(dag: Dag, gitRepoSettings: GitRepoSettings, actions: Seq[Action]) : UIO[Payload]
+    def createHeaders(gitRepoSettings: GitRepoSettings) : UIO[Map[String, String]]
+
+    def createRequest(dag: Dag, gitRepoSettings: GitRepoSettings): Task[GitRequest] = for {
+        actions <- createActions(dag, gitRepoSettings)
+        payload <- createPayload(dag, gitRepoSettings, actions)
+        headers <- createHeaders(gitRepoSettings)
+        request <- Task {
+            GitRequest(
+              gitRepoSettings.repository,
+              payload,
+              headers
+            )
+        }
+      } yield request
+   }
 
   case class GitRequest(uri: String, payload: Payload, headers: Map[String, String])
   case class Payload(branch: String, commit_message: String, actions: Seq[Action])
   case class Action(action: String, file_path: String, content: String)
 
 
-
-  def createRequest(dag: Dag, gitRepoSettings: GitRepoSettings): Task[GitRequest] = {
-
-    val request = for {
-      actions <- ZIO.succeed(Seq(Action("create", s"${gitRepoSettings.path}\\${dag.project}\\${dag.name}.yaml", raw"${dag.payload}")))
-      payload <- ZIO.succeed(Payload(gitRepoSettings.branch, s"from ${dag.project}", actions))
-      request <- Task {
-        {
-          GitRequest(
-            gitRepoSettings.repository,
-            payload,
-            Map("Content-Type" -> "application/json", "PRIVATE-TOKEN" -> gitRepoSettings.privateToken),
-          )
-        }
-      }
-    } yield request
-
-    request
-  }
 
   val live: ZLayer[Any, Nothing, Git] = ZLayer.fromEffect(
 
@@ -62,7 +58,20 @@ package object git {
             }
           } yield (response.body)
         }
+
+        override def createActions(dag: Dag, gitRepoSettings: GitRepoSettings): UIO[Seq[Action]] = ZIO.succeed(Seq(
+          Action("create", s"${gitRepoSettings.path}\\${dag.project}_${dag.name}.yaml", raw"${dag.payload}"))
+        )
+
+        override def createPayload(dag: Dag, gitRepoSettings: GitRepoSettings, actions: Seq[Action]): UIO[Payload] = ZIO.succeed(
+          Payload(gitRepoSettings.branch, s"from ${dag.project}", actions)
+        )
+
+        override def createHeaders(gitRepoSettings: GitRepoSettings): UIO[Map[String, String]] =  ZIO.succeed(
+          Map("Content-Type" -> "application/json", "PRIVATE-TOKEN" -> gitRepoSettings.privateToken)
+        )
       }
-    })
+    }
+  )
 
 }
