953627ff5d1fa00f0c8fe67ab17fc6b69f153831
va <va@pimpay.ru>
2020-11-02 22:47:20 +0300

    lazy E on getOrFail

--- src/main/scala/zio/sugar/zio.scala
+++ src/main/scala/zio/sugar/zio.scala
@@ -3,7 +3,7 @@ package zio
 package object sugar {
 
   implicit class OptionSyntax[A](v:Option[A]) {
-    def getOrFail[E](e:E):ZIO[Any, E, A] = ZIO.fromOption(v).orElseFail(e)
+    def getOrFail[E](e: => E):ZIO[Any, E, A] = ZIO.fromOption(v).orElseFail(e)
   }
 
   implicit class ZIOSyntax(z:ZIO.type) {
