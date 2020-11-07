59a2ef94a3e93b1536ce81360d06fbdcd8c38a81
va <va@pimpay.ru>
2020-11-07 22:10:21 +0300

    added sync project method into API

--- src/main/scala/crawler/package.scala
+++ src/main/scala/crawler/package.scala
@@ -1,7 +1,9 @@
 import domain._
-import zio.Task
+import zio.{Has, Task}
 
 package object crawler {
+  type Crawler = Has[Service]
+
   trait Service {
     def fetch(project:Project):Task[Map[DagName, DagPayload]]
   }
